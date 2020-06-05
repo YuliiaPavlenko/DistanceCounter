80ea6159a05e7e1bd67bada39854cca2b66b3303
Yuliia Pavlenko <>
2020-06-05 16:47:22 +0200

    Add custom fonts

---
+++ DistanceCounter/Common/Fonts.swift
@@ -0,0 +1,16 @@
+//
+//  Fonts.swift
+//  DistanceCounter
+//
+//  Created by Yuliia Pavlenko on 05/06/2020.
+//  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
+//
+
+import UIKit
+
+struct Fonts {
+    static let titleBold = UIFont.boldSystemFont(ofSize: 16)
+    static let titleRegular = UIFont.systemFont(ofSize: 16)
+    static let textFieldFont = UIFont.systemFont(ofSize: 12)
+    static let titleViewFont = UIFont.systemFont(ofSize: 14)
+}
