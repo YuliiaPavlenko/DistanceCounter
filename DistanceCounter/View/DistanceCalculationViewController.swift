5eb3b7dc14711f2ceac759e5bf06807f358d9d24
Yuliia Pavlenko <>
2020-06-05 16:48:47 +0200

    Add distance calculation vc

--- DistanceCounter/View/DistanceCalculationViewController.swift
+++ DistanceCounter/View/DistanceCalculationViewController.swift
@@ -9,22 +9,21 @@
 import UIKit
 
 class DistanceCalculationViewController: UIViewController {
-
+    
+    @IBOutlet weak var startPointTitleLabel: CustomLabel!
+    @IBOutlet weak var startPointLatitudeTextField: CustomTextField!
+    @IBOutlet weak var startPointLongitudeTextField: CustomTextField!
+    @IBOutlet weak var finishPointTitleLabel: CustomLabel!
+    @IBOutlet weak var finishPointLatitudeTextField: CustomTextField!
+    @IBOutlet weak var finishPointLongtitudeTextField: CustomTextField!
+    @IBOutlet weak var calculateDistanceButton: CustomButton!
+    @IBOutlet weak var distanceInKilometersTitleLabel: CustomLabel!
+    @IBOutlet weak var distanceInMetersTitleLabel: CustomLabel!
+    @IBOutlet weak var startPointInfoTextView: CustomTextView!
+    @IBOutlet weak var finishPointInfoTextView: CustomTextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
