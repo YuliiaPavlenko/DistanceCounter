49872146fd48fb3454d9179abae3c26f825778fa
Yuliia Pavlenko <>
2020-06-06 11:53:05 +0200

    Add strings file

---
+++ DistanceCounter/UI/Common/Utils/String+Utils.swift
@@ -0,0 +1,15 @@
+//
+//  String+Utils.swift
+//  DistanceCounter
+//
+//  Created by Yuliia Pavlenko on 06/06/2020.
+//  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func localized(bundle: Bundle = .main, tableName: String = "Localizable") -> String {
+        return NSLocalizedString(self, tableName: tableName, value: "**\(self)**", comment: "")
+    }
+}
