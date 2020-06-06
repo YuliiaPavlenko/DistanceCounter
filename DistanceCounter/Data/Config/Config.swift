4b785d4728c5c8672eb6879233704e2f2d82d1f3
Yuliia Pavlenko <>
2020-06-06 12:26:03 +0200

    Add api router

---
+++ DistanceCounter/Data/Config/Config.swift
@@ -0,0 +1,13 @@
+//
+//  Config.swift
+//  DistanceCounter
+//
+//  Created by Yuliia Pavlenko on 06/06/2020.
+//  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
+//
+
+import Foundation
+
+struct Config {
+    static let baseURL = "https://nominatim.openstreetmap.org"
+}
---
+++ DistanceCounter/Data/Networking/ApiRouter.swift
@@ -0,0 +1,17 @@
+//
+//  ApiRouter.swift
+//  DistanceCounter
+//
+//  Created by Yuliia Pavlenko on 06/06/2020.
+//  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
+//
+
+import Foundation
+
+class ApiRouter {
+    private static let format = Config.baseURL + "/reverse?format=jsonv2"
+    
+    static func pointFor(latitude: Double, longitute: Double) -> String {
+        return format + "&lat=\(latitude)&lon=\(longitute)"
+    }
+}
